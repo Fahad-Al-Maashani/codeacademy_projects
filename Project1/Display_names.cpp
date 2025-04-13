a58ca27c67772ab07a4595e6f519ab8d41184e5d
Fahad Al Maashani <122757915+Fahad-Al-Maashani@users.noreply.github.com>
2025-04-13 12:44:13 +0400

    Create Display_names.cpp

---
+++ Project1/Display_names.cpp
@@ -0,0 +1,15 @@
+#include<iostream> 
+int main(){
+// The given program will dispaly the actual output of my name display 
+std :: cout<<"FFFFFF                   A\n";
+std::cout<<"F                       A  A\n";
+std::cout<<"F                      A    A\n"; 
+std::cout<<"F                     AAAAAAAA\n";
+std::cout<<"FFFFFF               A        A\n";
+std::cout<<"F                   A          A\n"; 
+std::cout<<"F                  A            A\n";
+std::cout<<"F                 A              A\n";
+
+
+
+}
